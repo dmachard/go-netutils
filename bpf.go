9c1bcf0a2d2353f7dfbbac1eecbf8565dc245611
dmachard <5562930+dmachard@users.noreply.github.com>
2024-10-06 19:02:29 +0200

    update bpf filter with GRE support

--- bpf.go
+++ bpf.go
@@ -22,6 +22,7 @@ type LabeledInstruction struct {
 	Instruction    bpf.Instruction
 	SkipTrueLabel  string
 	SkipFalseLabel string
+	SkipLabel      string
 }
 
 // Helper to manage labels and calculate jumps
@@ -31,11 +32,24 @@ type LabelResolver struct {
 }
 
 // Add an instruction to the list
-func (lr *LabelResolver) Add(instr bpf.Instruction, gotoTrue, gotoFalse string) {
+func (lr *LabelResolver) Add(instr bpf.Instruction) {
+	lr.Instructions = append(lr.Instructions, LabeledInstruction{
+		Instruction: instr,
+	})
+}
+
+func (lr *LabelResolver) JumpTo(instr bpf.Instruction, jumpTo string) {
+	lr.Instructions = append(lr.Instructions, LabeledInstruction{
+		Instruction: instr,
+		SkipLabel:   jumpTo,
+	})
+}
+
+func (lr *LabelResolver) JumpIf(instr bpf.Instruction, onTrue, onFalse string) {
 	lr.Instructions = append(lr.Instructions, LabeledInstruction{
 		Instruction:    instr,
-		SkipTrueLabel:  gotoTrue,
-		SkipFalseLabel: gotoFalse,
+		SkipTrueLabel:  onTrue,
+		SkipFalseLabel: onFalse,
 	})
 }
 
@@ -68,62 +82,153 @@ func (lr *LabelResolver) ResolveJumps() ([]bpf.Instruction, error) {
 				jump.SkipFalse = uint8(labelPos - i - 1)
 			}
 			instr = jump
+		case bpf.Jump:
+			// Resolve SkipTrue based on label if provided
+			if labeledInstr.SkipLabel != "" {
+				labelPos, ok := lr.LabelMap[labeledInstr.SkipLabel]
+				if !ok {
+					return nil, fmt.Errorf("label %s not found for Skip", labeledInstr.SkipLabel)
+				}
+				jump.Skip = uint32(labelPos - i - 1)
+			}
+			instr = jump
 		}
 		finalInstructions[i] = instr
 	}
 	return finalInstructions, nil
 }
 
+const (
+	EthLen  = uint32(14)
+	IPv6Len = uint32(40)
+	GreLen  = uint32(4)
+)
+
 func GetBpfFilterPort(port int) ([]bpf.Instruction, error) {
 	bpfInstructions := &LabelResolver{LabelMap: make(map[string]int)}
 
-	// Read ethernet protocol type
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 12, Size: 2}, "", "") // load eth.type (2 bytes at offset 12) and push-it in register A
+	// IPv4, IPv6 protocol condition from ethernet layer
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: 12, Size: 2})                                            // load eth.type (2 bytes at offset 12) and push-it in register A
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "read_ipv4", "")              // If eth.type == IPv4, goto read_ipv4
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x86dd}, "read_ipv6", "ignore_packet") // If eth.type == IPv6, goto read_ipv6
 
-	// IPv4, IPv6 ou GRE packet condition ?
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "read_ipv4", "")              // If eth.type == IPv4, goto read_ipv4
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x86dd}, "read_ipv6", "ignore_packet") // If eth.type == IPv6, goto read_ipv6
-
-	// read IPv4 packet
+	// Read IPv4 layer
 	bpfInstructions.Label("read_ipv4")
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 23, Size: 1}, "", "")                                   // Load ip.proto (1 byte at offset 23) and push-it in register A
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv4_port", "")             // ip.proto == UDP ? goto read_port
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv4_port", "")              // ip.proto == TCP ? goto read_port
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2f}, "accept_packet", "ignore_packet") // ip.proto == GRE, goto read_gre else ignore packet
-	bpfInstructions.Label("read_ipv4_port")
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 20, Size: 2}, "", "")                                           // load flags and fragment offset (2 bytes at offset 20)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x1fff}, "accept_packet", "")                  // Only look at the last 13 bits of the data saved in regiter A, 0x1fff == 0001 1111 1111 1111 (fragment offset)
-	bpfInstructions.Add(bpf.LoadMemShift{Off: 14}, "", "")                                                    // Register X = ip header len * 4
-	bpfInstructions.Add(bpf.LoadIndirect{Off: 14, Size: 2}, "", "")                                           // Load source port in tcp or udp (2 bytes at offset x+14)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
-	bpfInstructions.Add(bpf.LoadIndirect{Off: 16, Size: 2}, "", "")                                           // Load destination port in tcp or udp  (2 bytes at offset x+16)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: EthLen + 6, Size: 2})                                            // load flags and fragment offset (2 bytes at offset 20)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x1fff}, "accept_packet", "")                // Only look at the last 13 bits of the data saved in regiter A, 0x1fff == 0001 1111 1111 1111 (fragment offset)
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: EthLen + 9, Size: 1})                                            // Load ip.proto (1 byte at offset 23) and push-it in register A
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv4_transport", "")              // ip.proto == UDP ? goto read_ipv4_transport
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv4_transport", "")               // ip.proto == TCP ? goto read_ipv4_transport
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2f}, "read_ipv4_gre_proto", "ignore_packet") // ip.proto == GRE, goto read_gre else ignore packet
+	bpfInstructions.Label("read_ipv4_transport")
+	bpfInstructions.Add(bpf.LoadMemShift{Off: EthLen})                                                           // Load IP header size in Register X
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen, Size: 2})                                                  // Load source port in tcp or udp (2 bytes at offset Register X+EthLen)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + 2, Size: 2})                                              // Load destination port in tcp or udp  (2 bytes at offset x+16)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
 
-	// read IPv6 packet
+	// Read GRE layer over IPv4
+	bpfInstructions.Label("read_ipv4_gre_proto")
+	bpfInstructions.Add(bpf.LoadMemShift{Off: EthLen})                                                                // Load IP header size in Register X
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + 2, Size: 2})                                                   // Load GRE protocol type from header
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "read_ipv4_gre_flags_ipv4", "ignore_packet") // If GRE proto == IPv4, goto read_gre_ipv4
+
+	bpfInstructions.Label("read_ipv4_gre_flags_ipv4")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen, Size: 2}) // Load GRE flags from header (2 bytes at offset 34)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x8000}, "has_C", "")
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x2000}, "has_K", "")
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0xA000}, "has_CK", "read_ipv4_gre_ipv4")
+	bpfInstructions.Label("has_C")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 2})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv4_gre_ipv4")
+	bpfInstructions.Label("has_K")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 4})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv4_gre_ipv4")
+	bpfInstructions.Label("has_CK")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 6})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv4_gre_ipv4")
+
+	bpfInstructions.Label("read_ipv4_gre_ipv4")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + 6, Size: 2})                                           // load flags and fragment offset (2 bytes at offset 20)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x1fff}, "accept_packet", "")                        // Only look at the last 13 bits of the data saved in regiter A, 0x1fff == 0001 1111 1111 1111 (fragment offset)
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + 9, Size: 1})                                           // Load ip.proto (1 byte at offset 23) and push-it in register A
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv4_gre_ipv4_transport", "")             // ip.proto == UDP ? goto read_ipv4_transport
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv4_gre_ipv4_transport", "ignore_packet") // ip.proto == TCP ? goto read_ipv4_transport
+	bpfInstructions.Label("read_ipv4_gre_ipv4_transport")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen, Size: 1})                                         // Load the byte at offset 14 into register A to get dynamix header size
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAnd, Val: 0x0F})                                          // A = A & 0x0F (keep only the lower 4 bits)
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpShiftLeft, Val: 2})                                       // A = A << 2 (shift left by 2 bits to multiply by 4)
+	bpfInstructions.Add(bpf.ALUOpX{Op: bpf.ALUOpAdd})                                                            // add register X+A
+	bpfInstructions.Add(bpf.TAX{})                                                                               // move register A to X
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen, Size: 2})                                         // Load source port in tcp or udp (2 bytes at offset Register X+EthLen)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + 2, Size: 2})                                     // Load destination port in tcp or udp  (2 bytes at offset x+16)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
+
+	// read IPv6 layer
 	bpfInstructions.Label("read_ipv6")
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 20, Size: 1}, "", "")                                   // Load ipv6.nxt (2 bytes at offset 12) and push-it in register A
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2c}, "accept_packet", "")              // fragment ?
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv6_port", "")             // ip.proto == UDP ?
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv6_port", "ignore_packet") // ip.proto == TCP ?
-	//bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2f}, "read_ipv6_gre", "ignore_packet")      // If nxt == GRE, goto read_gre
-	bpfInstructions.Label("read_ipv6_port")
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 54, Size: 2}, "", "")                                           // Load source port tcp or udp (2 bytes at offset 54)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 56, Size: 2}, "", "")                                           // Load destination port tcp or udp (2 bytes at offset 56)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: EthLen + 6, Size: 1})                                            // Load ipv6.nxt (2 bytes at offset 12) and push-it in register A
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2c}, "accept_packet", "")                    // fragment ?
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv6_transport", "")              // ip.proto == UDP ?
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv6_transport", "ignore_packet")  // ip.proto == TCP ?
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2f}, "read_ipv6_gre_proto", "ignore_packet") // ip.proto == GRE, goto read_gre else ignore packet
+	bpfInstructions.Label("read_ipv6_transport")
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: EthLen + IPv6Len, Size: 2})                                        // Load source port tcp or udp (2 bytes at offset 54)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadAbsolute{Off: EthLen + IPv6Len + 2, Size: 2})                                    // Load destination port tcp or udp (2 bytes at offset 56)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
 
-	// Read GRE packet
-	bpfInstructions.Label("read_ipv4_gre")
-	bpfInstructions.Add(bpf.LoadAbsolute{Off: 36, Size: 2}, "", "")                        // Load GRE protocol type from header (2 bytes at offset 36)
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "accept_packet", "") // If GRE proto == IPv4, goto read_gre_ipv4
+	// Read GRE layer over IPv6
+	bpfInstructions.Label("read_ipv6_gre_proto")
+	bpfInstructions.Add(bpf.LoadMemShift{Off: EthLen})                                                                // Load IP header size in Register X
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + 2, Size: 2})                                                   // Load GRE protocol type from header
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x86dd}, "read_ipv4_gre_flags_ipv6", "ignore_packet") // If GRE proto == IPv4, goto read_gre_ipv4
+
+	bpfInstructions.Label("read_ipv4_gre_flags_ipv6")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen, Size: 2}) // Load GRE flags from header (2 bytes at offset 34)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x8000}, "has_C", "")
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0x2000}, "has_K", "")
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpBitsSet, Val: 0xA000}, "has_CK", "read_ipv6_gre_ipv6")
+	bpfInstructions.Label("has_C")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 2})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv6_gre_ipv6")
+	bpfInstructions.Label("has_K")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 4})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv6_gre_ipv6")
+	bpfInstructions.Label("has_CK")
+	bpfInstructions.Add(bpf.TXA{})
+	bpfInstructions.Add(bpf.ALUOpConstant{Op: bpf.ALUOpAdd, Val: 6})
+	bpfInstructions.Add(bpf.TAX{})
+	bpfInstructions.JumpTo(bpf.Jump{}, "read_ipv6_gre_ipv6")
+
+	bpfInstructions.Label("read_ipv6_gre_ipv6")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + 6, Size: 1})                                           // Load ipv6.nxt (2 bytes at offset 12) and push-it in register A
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x2c}, "accept_packet", "")                            // fragment ?
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x11}, "read_ipv6_gre_ipv6_transport", "")             // ip.proto == UDP ? goto read_ipv6_gre_ipv6_transport
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x6}, "read_ipv6_gre_ipv6_transport", "ignore_packet") // ip.proto == TCP ? goto read_ipv6_gre_ipv6_transport
+	bpfInstructions.Label("read_ipv6_gre_ipv6_transport")
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + IPv6Len, Size: 2})                               // Load source port in tcp or udp (2 bytes at offset Register X+EthLen)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "")              // source port equal to 53 ?
+	bpfInstructions.Add(bpf.LoadIndirect{Off: EthLen + GreLen + IPv6Len + 2, Size: 2})                           // Load destination port in tcp or udp  (2 bytes at offset x+16)
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: uint32(port)}, "accept_packet", "ignore_packet") // destination port equal to 53 ?
 
 	// Keep the packet and send up to 65k of the packet to userspace
 	bpfInstructions.Label("accept_packet")
-	bpfInstructions.Add(bpf.RetConstant{Val: 0xFFFF}, "", "")
+	bpfInstructions.Add(bpf.RetConstant{Val: 0xFFFF})
 
 	// Ignore packet
 	bpfInstructions.Label("ignore_packet")
-	bpfInstructions.Add(bpf.RetConstant{Val: 0}, "", "")
+	bpfInstructions.Add(bpf.RetConstant{Val: 0})
 
 	// Resolve and return final list of instructions
 	return bpfInstructions.ResolveJumps()
--- bpf_test.go
+++ bpf_test.go
@@ -11,11 +11,11 @@ func TestLabelResolver(t *testing.T) {
 	bpfInstructions := &LabelResolver{LabelMap: make(map[string]int)}
 
 	// Define some simple instructions with labels
-	bpfInstructions.Add(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "label_true", "label_false")
+	bpfInstructions.JumpIf(bpf.JumpIf{Cond: bpf.JumpEqual, Val: 0x0800}, "label_true", "label_false")
 	bpfInstructions.Label("label_true")
-	bpfInstructions.Add(bpf.RetConstant{Val: 0xFFFF}, "", "") // Accept packet
+	bpfInstructions.Add(bpf.RetConstant{Val: 0xFFFF}) // Accept packet
 	bpfInstructions.Label("label_false")
-	bpfInstructions.Add(bpf.RetConstant{Val: 0}, "", "") // Ignore packet
+	bpfInstructions.Add(bpf.RetConstant{Val: 0}) // Ignore packet
 
 	// Resolve the instructions
 	resolved, err := bpfInstructions.ResolveJumps()
